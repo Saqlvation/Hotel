4394b506f9e02e45566cfc9c3678847dddd86de9
Michael <92533988+Saqlvation@users.noreply.github.com>
2026-01-30 11:41:13 +0100

    Add TestHotel class to test Hotel functionality

---
+++ TestHotel.java
@@ -0,0 +1,9 @@
+public class TestHotel {
+    public static void main(String[] args) {
+        
+        Hotel x;
+        int[] vettore = {5, 3, 3 ,2,2,1};
+        x = new Hotel("Super hotel",vettore);
+        System.out.println(x);
+    }
+}
